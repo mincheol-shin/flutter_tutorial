38a1b719cba64f1c78d35cdb362c0c84abcfcadd
toast-ceo <70907737+toast-ceo@users.noreply.github.com>
2020-11-01 00:43:29 +0900

    final version
    사진, 작가, 대출&반납날짜, 책제목까지 나타내는 listview ui를 만들었습니다. 사진은 둥근 모양으로 될 수 있게 만들었습니다. / 추가적으로 궁금한건 멘토님이 책제목 옆에 칸을 만들어서 D Day를 표시해 주는것 fit을 공부해봤는데 여쭙고 싶은 것들이 있습니다. 그리고 api로 받아서 이미지를 줄려고 할 때는 이미지의 이름같은 부분에 대해서 어떻게 처리해야하는지 궁금합니다

--- third_week_practice_app/lib/main.dart
+++ third_week_practice_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main()=> runApp(BookScroll());
+void main() => runApp(BookScroll());
 
 class BookScroll extends StatefulWidget {
   @override
@@ -8,10 +8,50 @@ class BookScroll extends StatefulWidget {
 }
 
 class _BookScrollState extends State<BookScroll> {
-
-  final List<Map<String, dynamic>> book_list=[
-      {'name': '꾸뻬 씨의 행복 여행', 'retrunday': '2020/1/30', 'borrowday': '2020/1/22', 'writer': '프랑수아 를로르'},
-
+  /// TODO
+  final List<Map<String, dynamic>> book_list = [
+    {
+      'name': '꾸뻬 씨의 행복 여행',
+      'retrunday': '2020/1/30',
+      'borrowday': '2020/1/22',
+      'writer': '프랑수아 를로르',
+      'image': 'assets/happy.jpeg'
+    },
+    {
+      'name': '모든 순간이 너였다',
+      'retrunday': '2020/6/24',
+      'borrowday': '2020/6/17',
+      'writer': '하태완',
+      'image': 'assets/always.jpeg'
+    },
+    {
+      'name': '꾸뻬 씨의 행복 여행',
+      'retrunday': '2020/1/30',
+      'borrowday': '2020/1/22',
+      'writer': '프랑수아 를로르',
+      'image': 'assets/happy.jpeg'
+    },
+    {
+      'name': '모든 순간이 너였다',
+      'retrunday': '2020/6/24',
+      'borrowday': '2020/6/17',
+      'writer': '하태완',
+      'image': 'assets/always.jpeg'
+    },
+    {
+      'name': '꾸뻬 씨의 행복 여행',
+      'retrunday': '2020/1/30',
+      'borrowday': '2020/1/22',
+      'writer': '프랑수아 를로르',
+      'image': 'assets/happy.jpeg'
+    },
+    {
+      'name': '모든 순간이 너였다',
+      'retrunday': '2020/6/24',
+      'borrowday': '2020/6/17',
+      'writer': '하태완',
+      'image': 'assets/always.jpeg'
+    },
   ];
 
   @override
@@ -26,62 +66,76 @@ class _BookScrollState extends State<BookScroll> {
             elevation: 0.0,
             centerTitle: true,
             backgroundColor: Colors.white,
-            title: Center(
-                child: Text(
+            title: Text(
               'Flutter Tutorial',
               style: TextStyle(color: Colors.black),
-            )),
+            ),
           ),
-
           body: ListView.builder(
             itemCount: book_list.length,
             itemBuilder: (context, index) {
               return Container(
                 height: 150.0,
-                  child: Row(
-                    children: <Widget>[
-                      Flexible(
-                      flex:3,
-
-                  ),
-                      Flexible(
-                        flex:7,
-                        child: Padding(
-                          padding: const EdgeInsets.only(left:10.0,),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text("${book_list[index]["name"]}",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18.0,
-                                  color: Colors.black,
-                                  fontFamily: 'BlackHanSans',
-                                ),
+                child: Row(
+                  children: <Widget>[
+                    Flexible(
+                      flex: 3,
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Column(
+                          children: <Widget>[
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: Image.asset(
+                                '${book_list[index]["image"]}',
+                                height: 120,
+                                width: 120,
+                                fit: BoxFit.fill,
                               ),
-                              Text(
-                                "${book_list[index]["writer"]}",
-                                style: TextStyle(
-                                  fontSize: 12.0,
-                                  color: Colors.grey,
-                                  fontFamily: 'BlackHanSans',
-                                ),
-                              ),
-                              const SizedBox(height: 80.0),
-                              Text(
-                                "대출일 : ${book_list[index]["borrowday"]} ~ 반납일 : ${book_list[index]["retrunday"]}",
-                                style: TextStyle(
-                                  fontSize: 10.0,
-                                  color: Colors.grey,
-                                  fontFamily: 'BlackHanSans',
-                                ),
-                              ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    const SizedBox(height: 10.0),
+                    Flexible(
+                      flex: 7,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          left: 10.0,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "\n${book_list[index]["name"]}",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18.0,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Text(
+                              "${book_list[index]["writer"]}",
+                              style: TextStyle(
+                                fontSize: 12.0,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            const SizedBox(height: 60.0),
+                            Text(
+                              "대출일 : ${book_list[index]["borrowday"]} ~ 반납일 : ${book_list[index]["retrunday"]}",
+                              style: TextStyle(
+                                fontSize: 10.0,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               );
             },
           )),
