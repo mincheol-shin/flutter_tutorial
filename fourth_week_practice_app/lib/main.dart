24544fdfe5003fbab2f8f2d744d318fc0e9e5320
toast-ceo <70907737+toast-ceo@users.noreply.github.com>
2020-11-06 01:50:20 +0900

    screen
    1~4스크린을 넘길 수 있게 만들었습니다

--- fourth_week_practice_app/lib/main.dart
+++ fourth_week_practice_app/lib/main.dart
@@ -11,19 +11,100 @@ class _PageState extends State<Page> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Container(
         child: PageView(
           children: <Widget>[
             SizedBox.expand(
               child: Container(
-                color: Colors.white,
                 child: Center(
-                  child: Text(" Introduction Screen",
-                    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Introduction Screen",
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 20.0),
+                      Text(
+                        " 1 번째 스크린",
+                        style: TextStyle(
+                            fontSize: 15.0),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox.expand(
+              child: Container(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Introduction Screen",
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 20.0),
+                      Text(
+                        " 2 번째 스크린",
+                        style: TextStyle(
+                            fontSize: 15.0),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox.expand(
+              child: Container(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Introduction Screen",
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 20.0),
+                      Text(
+                        " 3 번째 스크린",
+                        style: TextStyle(
+                            fontSize: 15.0),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox.expand(
+              child: Container(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Introduction Screen",
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 20.0),
+                      Text(
+                        " 4 번째 스크린",
+                        style: TextStyle(
+                            fontSize: 15.0),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
           ],
+
         ),
       ),
     );
